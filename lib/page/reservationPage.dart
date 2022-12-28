35ffeb6576caf618aaae843632de744cf03df214
Willy Awkward <willy1118t@gmail.com>
2022-12-28 16:10:26 +0800

    [Add] Reservation page

--- lib/page/reservationPage.dart
+++ lib/page/reservationPage.dart
@@ -1,28 +1,53 @@
-// ignore_for_file: camel_case_types, file_names
-
 import 'package:flutter/material.dart';
+import 'package:ncu_reservation_app/page/addReservation.dart';
 
-class loadingPage extends StatelessWidget {
-  const loadingPage({Key? key}) : super(key: key);
+class reservationPage extends StatefulWidget {
+  const reservationPage({Key? key}) : super(key: key);
 
+  @override
+  State<reservationPage> createState() => _loadingPageState();
+}
+
+class _loadingPageState extends State<reservationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            Color.fromARGB(255, 0, 4, 4),
-            Colors.white,
-          ],
-        )),
-        child: const Center(
-          child: Text("Hello"),
-        ),
-      )),
+      backgroundColor: const Color.fromARGB(255, 223, 234, 233),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Text(
+                "預約時段",
+                style: TextStyle(
+                  fontSize: 48,
+                  fontFamily: 'ChenYuluoyan-Thin',
+                ),
+              ),
+              IconButton(
+                icon: const Icon(Icons.add),
+                iconSize: 30,
+                splashRadius: 20,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const addReservation(),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+        ],
+      ),
     );
   }
 }
